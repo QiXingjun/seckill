f9343f3006a030b73e72e1b5534be176b3fbe915
houwanxing1 <houwanxing1@jd.com>
2017-09-12 18:16:30 +0800

    增加输出内容。

--- src/main/java/com/seckill/dao/cache/HelloGit.java
+++ src/main/java/com/seckill/dao/cache/HelloGit.java
@@ -2,6 +2,6 @@ package com.seckill.dao.cache;
 
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello,Git.");
+        System.out.println("hello,git.");
     }
 }
