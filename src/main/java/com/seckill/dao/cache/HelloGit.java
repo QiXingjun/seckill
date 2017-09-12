665ec72d10c0c9982a6c91e0a29bc5f23598598b
hwx-shutter <605181715@qq.com>
2017-09-12 18:33:01 +0800

    Update HelloGit.java

--- src/main/java/com/seckill/dao/cache/HelloGit.java
+++ src/main/java/com/seckill/dao/cache/HelloGit.java
@@ -2,6 +2,7 @@ package com.seckill.dao.cache;
 
 public class HelloGit {
     public static void main(String[] args) {
+        System.out.println("add new content2.");
         System.out.println("hello,git.");
         System.out.println("add new content.");
     }
