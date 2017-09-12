cb5241bdc015992c53a40537851efb03fd8b4233
hwx-shutter <605181715@qq.com>
2017-09-12 18:18:34 +0800

    Update HelloGit.java
    新增内容输出。

--- src/main/java/com/seckill/dao/cache/HelloGit.java
+++ src/main/java/com/seckill/dao/cache/HelloGit.java
@@ -3,5 +3,6 @@ package com.seckill.dao.cache;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello,git.");
+        System.out.println("add new content.");
     }
 }
